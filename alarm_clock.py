c92a83a77680b7d134d8185b107e5b4694f36df6
Saint <ss.mscx@gmail.com>
2026-06-19 23:36:21 -0600

    Primera prueba de funcionamiento de la función

--- alarm_clock.py
+++ alarm_clock.py
@@ -8,3 +8,11 @@ def alarm(seconds):
         #Esto hace enfasis a cque despues de cierto tiempo la alarma va a sonar las veces que el dispositivo lo permita
         time.sleep(1)
         time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"Tiempo restante: {minutes_left} minutos y {seconds_left} segundos")
+
+alarm(10)
